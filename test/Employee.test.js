6cc4e374cfee45dfd2814851740d661202cefa96
God of War <jamel.eggleston@gmail.com>
2021-10-12 21:34:52 -0400

    updated employee and engineer test

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -11,21 +11,21 @@ describe('Employee', () => {
 
     it("should check for a name given", () => {
         const newHire = new Employee("The Hulk",1,"rager@mail.com");
-        expect(typeof (newHire.getName())).toEqual("The Hulk");
+        expect(newHire.getName()).toEqual("The Hulk");
     });
 
     it("should check for a id given", () => {
         const newHire = new Employee("Thor", 81, "thunderlord@mail.com");
-        expect(typeof (newHire.getId())).toEqual(81);
+        expect(newHire.getId()).toEqual(81);
     });
 
     it("should check for a email given", () => {
         const newHire = new Employee("Captain America",47,"starsandstripes@mail.com");
-        expect(typeof (newHire.getEmail())).toEqual("starsandstripes@mail.com");
+        expect(newHire.getEmail()).toEqual("starsandstripes@mail.com");
     });
 
     it("should check for a emplyee's role", () =>{
         const newHire = new Employee("Hawkeye", 44, "bowsandarrows@mail.com");
-        expect(typeof (newHire.getRole())).toEqual("Employee")
+        expect(newHire.getRole()).toEqual("Employee")
     });
 })
--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,1 +1,34 @@
-//Start Here
+const { it } = require('@jest/globals');
+const Engineer = require('../lib/Engineer');
+
+describe('Engineer', () => {
+    it('should return an Engineer instance', () => {
+        const newHire = new Engineer("Ironman",0,"atombomb@mail.com");
+        expect(typeof(newHire)).toEqual("object");
+    });
+
+    it('should check for a name given', () => {
+        const newHire = new Engineer("The Hulk", 1, "rager@mail.com");
+        expect(newHire.getName()).toEqual("The Hulk");
+    });
+
+    it('should check for a id given', () => {
+        const newHire = new Engineer("Thor", 81, "thunderlord@mail.com");
+        expect(newHire.getId()).toEqual(81);
+    });
+
+    it('should check for a email given', () => {
+        const newHire = new Engineer("Captain America", 47, "starsandstripes@mail.com");
+        expect(newHire.getEmail()).toEqual("starsandstripes@mail.com");
+    });
+
+    it("should check for the Engineer's role", () => {
+        const newHire = new Engineer("Hawkeye", 44, "bowsandarrows@mail.com");
+        expect(newHire.getRole()).toEqual("Engineer");
+    })
+
+    it("should check for the Engineer's github username", () => {
+        const newHire = new Engineer("Black Widow", 25, "gunsandfist@mail.com", "blackspiderbite");
+        expect(newHire.getGithub()).toEqual("blackspiderbite");
+    });
+})
