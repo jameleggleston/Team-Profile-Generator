9e0fdc31ee82bc882fe52196221562252ad6cb6d
God of War <jamel.eggleston@gmail.com>
2021-10-12 20:15:51 -0400

    updated Employee test file

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,1 +1,31 @@
-//Start here
+const Employee = require('../lib/Employee');
+
+// This will test the Employee class bases on parameter inputs
+
+describe('Employee', () => {
+    
+    it("should return an Employee instance", () => {
+        const newHire = new Employee("Ironman",0,"atombomb@mail.com");
+        expect(typeof (newHire)).toEqual("object");
+    });
+
+    it("should check for a name given", () => {
+        const newHire = new Employee("The Hulk",1,"rager@mail.com");
+        expect(typeof (newHire.getName())).toEqual("The Hulk");
+    });
+
+    it("should check for a id given", () => {
+        const newHire = new Employee("Thor", 81, "thunderlord@mail.com");
+        expect(typeof (newHire.getId())).toEqual(81);
+    });
+
+    it("should check for a email given", () => {
+        const newHire = new Employee("Captain America",47,"starsandstripes@mail.com");
+        expect(typeof (newHire.getEmail())).toEqual("starsandstripes@mail.com");
+    });
+
+    it("should check for a emplyee's role", () =>{
+        const newHire = new Employee("Hawkeye", 44, "bowsandarrows@mail.com");
+        expect(typeof (newHire.getRole())).toEqual("Employee")
+    });
+})
