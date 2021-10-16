7757c4a5f4d600d05335c9c356003effaeca8da8
God of War <jamel.eggleston@gmail.com>
2021-10-16 00:59:14 -0400

    added google font

--- src/createHTML.js
+++ src/createHTML.js
@@ -69,6 +69,7 @@ function finalDocument(emplyeeCards){
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sue+Ellen+Francisco">
         <link rel="stylesheet" href="../dist/style.css">
         <title>Team Profile</title>
     </head>
